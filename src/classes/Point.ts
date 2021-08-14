5d2725009557f03f0565bfee75d9e59506cd36d8
Reza Mirjahanian <rmirjahanian@gmail.com>
2021-08-14 12:12:53 +0430

    class practice

--- src/classes/Point.ts
+++ src/classes/Point.ts
@@ -1,25 +1,59 @@
 class Point {
 
-  constructor(private x: number, private y: number) {
-
-  }
+  constructor(protected _x: number, protected _y: number) {}
 
   add(point: Point) {
-    return new Point(point.x + this.x, point.y + this.y);
+    this._x += point.X;
+    this._y += point.Y;
   }
 
-  print(){
-      console.log(`X${this.x},Y:${this.y}`)
+  get X() {
+    return this._x;
+  }
+
+  get Y() {
+    return this._y;
+  }
+
+  print() {
+    console.log(`X:${this.X},Y:${this.Y}`);
   }
 
 }
 
-const run = ()=>{
-    const point1 = new Point(1,2);
-    const point2 = new Point(4,5);
-    point2.add(point1);
-    point2.print();
+class Point3D extends Point {
+  constructor(protected _x: number, protected _y: number, private _z: number) {
+    super(_x, _y);
+
+  }
+
+  add(point: Point3D) {
+    super.add(point);
+    this._z += point.Z;
+  }
+
+  get Z() {
+    return this._z;
+  }
+
+
+  print() {
+    console.log(`X:${this.X},Y:${this.Y},Z:${this.Z}`);
+  }
+}
+
+const run = () => {
+  const point1 = new Point(1, 2);
+  const point2 = new Point(4, 5);
+  point2.add(point1);
+  point2.print();
+  //
+  const point3d1 = new Point3D(3, 4, 5);
+  const point3d2 = new Point3D(1, 1, 1);
+  point3d1.add(point3d2);
+  point3d1.print();
+
 
 }
 
-export default  run;
+export default run;
